94b82cc296d9c654747c794540ca8ad1df1abc16
huasmc <huasmc@gmail.com>
2023-01-08 21:09:37 -0400

    feat: remove logs

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,54 +1,106 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import server from "./server";
+import { utf8ToBytes, toHex } from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
+import * as secp from "ethereum-cryptography/secp256k1";
 
-function Transfer({ address, setBalance }) {
-  const [sendAmount, setSendAmount] = useState("");
-  const [recipient, setRecipient] = useState("");
+function Transfer({ privateKey, setBalance }) {
+	const [sendAmount, setSendAmount] = useState("");
+	const [recipient, setRecipient] = useState("");
+	const [isSigned, setIsSigned] = useState(false);
+	const [signature, setSignature] = useState(null);
+	const setValue = (setter) => (evt) => setter(evt.target.value);
 
-  const setValue = (setter) => (evt) => setter(evt.target.value);
+	async function transfer(evt) {
+		evt.preventDefault();
+		try {
+			const {
+				data: { balance },
+			} = await server.post(`send`, signature);
+			setBalance(balance);
+			setIsSigned(false);
+			setSignature(null);
+		} catch (ex) {
+			alert(ex.response.data.message);
+		}
+	}
 
-  async function transfer(evt) {
-    evt.preventDefault();
+	async function sign() {
+		if (recipient && sendAmount) {
+			const publicKey = secp.getPublicKey(privateKey);
+			const transaction = {
+				sender: toHex(publicKey),
+				recipient,
+				amount: parseInt(sendAmount),
+			};
+			const transactionBytes = utf8ToBytes(JSON.stringify(transaction));
+			const hashedTransaction = keccak256(transactionBytes);
+			// Recovered or recovery bit allows to recover public key from signature
+			const [signature, recoveryBit] = await secp.sign(
+				hashedTransaction,
+				privateKey,
+				{
+					recovered: true,
+				}
+			);
+			// Server receives Uint8Array as object to be converted
+			setSignature({
+				signatureObject: signature,
+				recoveryBit,
+				transaction,
+				publicKeyObject: publicKey,
+			});
+			setIsSigned(true);
+		}
+	}
 
-    try {
-      const {
-        data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
-      setBalance(balance);
-    } catch (ex) {
-      alert(ex.response.data.message);
-    }
-  }
+	useEffect(() => {
+		setIsSigned(false);
+	}, [privateKey, recipient, sendAmount]);
 
-  return (
-    <form className="container transfer" onSubmit={transfer}>
-      <h1>Send Transaction</h1>
+	return (
+		<form className="container transfer" onSubmit={transfer}>
+			<h1>Send Transaction</h1>
 
-      <label>
-        Send Amount
-        <input
-          placeholder="1, 2, 3..."
-          value={sendAmount}
-          onChange={setValue(setSendAmount)}
-        ></input>
-      </label>
+			<label>
+				Send Amount
+				<input
+					placeholder="1, 2, 3..."
+					value={sendAmount}
+					onChange={setValue(setSendAmount)}
+				></input>
+			</label>
 
-      <label>
-        Recipient
-        <input
-          placeholder="Type an address, for example: 0x2"
-          value={recipient}
-          onChange={setValue(setRecipient)}
-        ></input>
-      </label>
+			<label>
+				Recipient
+				<input
+					placeholder="Type an address, for example: 0x2"
+					value={recipient}
+					onChange={setValue(setRecipient)}
+				></input>
+			</label>
 
-      <input type="submit" className="button" value="Transfer" />
-    </form>
-  );
+			<button
+				type="button"
+				className="button"
+				onClick={sign}
+				style={{
+					backgroundColor: privateKey && sendAmount && recipient ? "" : "gray",
+				}}
+			>
+				Sign Transaction
+			</button>
+
+			<input
+				type="submit"
+				className="button"
+				value="Transfer"
+				style={{ backgroundColor: isSigned ? "" : "gray" }}
+				disabled={!isSigned}
+			/>
+			{isSigned && <p style={{ color: "green" }}>Succesfully signed</p>}
+		</form>
+	);
 }
 
 export default Transfer;
--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,31 +1,53 @@
+import { useState } from "react";
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
-  async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
-    if (address) {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
-    } else {
-      setBalance(0);
-    }
-  }
+function Wallet({ privateKey, setPrivateKey, balance, setBalance }) {
+	const [isKeyInvalid, setIsKeyInvalid] = useState(false);
+	const [address, setAddress] = useState("");
+	async function onChange(evt) {
+		const privateKey = evt.target.value;
+		if (privateKey.length >= 64) {
+			setPrivateKey(privateKey);
+			let publicKey = secp.getPublicKey(privateKey);
+			setAddress(toHex(publicKey));
+			setIsKeyInvalid(false);
+		} else setIsKeyInvalid(true);
+		setPrivateKey(privateKey);
+		if (privateKey) {
+			const {
+				data: { balance },
+			} = await server.get(`balance/${privateKey}`);
+			setBalance(balance);
+		} else {
+			setBalance(0);
+		}
+	}
 
-  return (
-    <div className="container wallet">
-      <h1>Your Wallet</h1>
+	return (
+		<div className="container wallet">
+			<h1>Your Wallet</h1>
 
-      <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
-      </label>
+			<label>
+				Private Key
+				<input
+					placeholder="(Testing purposes) add private key"
+					value={privateKey}
+					onChange={onChange}
+				></input>
+			</label>
 
-      <div className="balance">Balance: {balance}</div>
-    </div>
-  );
+			<p>Address {address.slice(0, 10)}...</p>
+
+			<div className="balance">Balance: {balance}</div>
+			{isKeyInvalid && privateKey.length ? (
+				<p style={{ color: "red" }}>Invalid Key</p>
+			) : (
+				""
+			)}
+		</div>
+	);
 }
 
 export default Wallet;
